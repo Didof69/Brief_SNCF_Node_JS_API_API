532ee094160f29ede2c9358c95bb5e3f99e30794
zguen <guillaume.mousin@gmail.com>
2023-08-29 16:03:08 +0200

    test encore

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,12 +1,6 @@
 import AppDataSource from "../data-source";
 import { User } from "../entities/User";
 
-<<<<<<< HEAD
-export class PlantService {
-    private plantRepository= AppDataSource.getRepository(User)
-}
-=======
 export class UserService {
   private UserRepository = AppDataSource.getRepository(User);
 }
->>>>>>> d6ff4ee679d104389e1acbda48acf8d3cc11e742

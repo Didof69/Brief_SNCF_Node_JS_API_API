3c98bd337e074e5ca5c54ffe98d8dae81aa90f6c
Lydie Chevalier <didof69@hotmail.fr>
2023-08-29 14:06:54 +0200

    1 - squelette + index + data-source

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('hello world!');
+import express, { Response, Request } from "express";
+import plantRouter from "./routers/PlantRouter";
+import AppDataSource from "./data-source";
+import userRouter from "./routers/UserRouter";
+
+//.then() permet d'attendre que l'initialisation soit faite (asynchrone)
+AppDataSource.initialize()
+  .then(() => {
+    const app = express();
+
+    //permet à notre API d'accepter des données en entrées en json
+    app.use(express.json());
+
+    //va rediriger les requetes de localhost:3000/api/books
+    app.use("/api/plants", plantRouter);
+    app.use("/api/users", userRouter);
+
+    app.listen(3000, () => {
+      console.log("application correctement lancée sur le port 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });

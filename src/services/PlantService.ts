8b7fef6f6f87e4231efde4d0a1dfd4911a666651
Lydie Chevalier <didof69@hotmail.fr>
2023-08-30 09:41:50 +0200

    deletePlant OK

--- src/services/PlantService.ts
+++ src/services/PlantService.ts
@@ -7,6 +7,6 @@ export class PlantService {
   private plantRepository = AppDataSource.getRepository(Plant);
 
   async delete(id: number) {
-      return await this.plantRepository.delete({id})
+    return await this.plantRepository.delete({ id });
   }
 }
